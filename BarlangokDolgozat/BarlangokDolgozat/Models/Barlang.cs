4c69ac5b18b4508fdb933c3a8eb6ed3a2bab6b9c
HKristof25 <21.hajdu.kristof@mechwart.com>
2025-09-30 08:13:26 +0200

    f1 -f2

---
+++ BarlangokDolgozat/BarlangokDolgozat/Models/Barlang.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarlangokDolgozat.Models
+{
+    public class Barlang
+    {
+        [Key]
+        public int id { get; set; }
+        public string nev { get; set; }
+        public int kiterjedes { get; set; }
+        public int melyseg {  get; set; }
+
+        public int magassag {  get; set; }
+        public string telepules { get; set; }
+    }
+}
--- BarlangokDolgozat/BarlangokDolgozat/obj/Debug/net8.0/BarlangokDolgozat.AssemblyInfo.cs
+++ BarlangokDolgozat/BarlangokDolgozat/obj/Debug/net8.0/BarlangokDolgozat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarlangokDolgozat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ab0e00882bff41a634aab92ebf7a6c85e4a111")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarlangokDolgozat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarlangokDolgozat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
