462be4b57f9404e49e1527afcd9c5cddea508377
HKristof25 <21.hajdu.kristof@mechwart.com>
2025-09-30 09:06:23 +0200

    újra kellet kezdeni az egészet mert ez a vs nem volt hajlandó legenerálni a .db fájt

---
+++ BarlangokD/BarlangokD/Data/BarlangDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BarlangokD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarlangokD.Data
+{
+    public class BarlangDbContext : DbContext
+    {
+        public BarlangDbContext(DbContextOptions<BarlangDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<barlang> barlangok {  get; set; }
+    }
+}
---
+++ BarlangokD/BarlangokD/Models/barlang.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarlangokD.Models
+{
+    public class barlang
+    {
+        [Key]
+        public int id { get; set; }
+        public string nev { get; set; }
+        public int kiterjedes { get; set; }
+        public int melyseg { get; set; }
+
+        public int magassag { get; set; }
+        public string telepules { get; set; }
+    }
+}
